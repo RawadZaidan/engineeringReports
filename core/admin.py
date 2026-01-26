aeda3aad5c8ad6cc6edb4b5882f9ecd58d163bd3
Rawad Zaidan <Rawad.Zaidan@Medilab.local>
2026-01-26 13:03:15 +0200

    first update

---
+++ core/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from .models import Product, ServiceReport, ReportItem, ReportImage, SavedFilter
+
+class ReportItemInline(admin.TabularInline):
+    model = ReportItem
+    extra = 0
+
+class ReportImageInline(admin.TabularInline):
+    model = ReportImage
+    extra = 0
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'manufacturer', 'model', 'category')
+    search_fields = ('name', 'model', 'manufacturer')
+    list_filter = ('category',)
+
+@admin.register(ServiceReport)
+class ServiceReportAdmin(admin.ModelAdmin):
+    list_display = ('id', 'client_name', 'location', 'service_date', 'engineer', 'status')
+    list_filter = ('status', 'service_date', 'engineer')
+    search_fields = ('client_name', 'location', 'issue_description')
+    inlines = [ReportItemInline, ReportImageInline]
+    readonly_fields = ('created_at', 'updated_at')
+
+admin.site.register(ReportItem)
+admin.site.register(ReportImage)
+
+@admin.register(SavedFilter)
+class SavedFilterAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'filter_type', 'is_default', 'created_at')
+    list_filter = ('filter_type', 'is_default')
+    search_fields = ('name', 'user__username')
